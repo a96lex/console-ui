329e8246be0210f822b15ce38659adbd887a47d8
a96lex <a96lex@gmail.com>
2023-08-02 11:28:21 +0200

    feat: adde verything to app

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
   import UserForm from "./lib/UserForm.svelte";
+  import PreviewContainer from "./lib/previews/PreviewContainer.svelte";
 </script>
 
 <main>
   <UserForm />
+  <PreviewContainer />
 </main>
+
+<style lang="scss">
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100%;
+  }
+</style>
