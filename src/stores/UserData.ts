4b030e0b9ad12b3216337aa49c1fc417f95f5622
a96lex <a96lex@gmail.com>
2023-08-02 11:27:23 +0200

    add: dummy data and new store for response

--- src/stores/UserData.ts
+++ src/stores/UserData.ts
@@ -1,3 +1,29 @@
 import { type Writable, writable } from "svelte/store";
 
 export const previewLoading: Writable<boolean> = writable(false);
+
+const testData = {
+  profile: {
+    person: {
+      name: "alex",
+      title: "alex",
+      seniority: "alex",
+      departments: "alex",
+    },
+    company: {
+      name: "alex",
+      description: "alex",
+      location: "alex",
+    },
+  },
+  message: `hello alex
+i am alex
+
+i sell some cookies do u want? If bulk, get discount!!
+
+[Object Object]`,
+};
+
+export const previewData: Writable<IPreviewData[]> = writable(
+  Array(3).fill(testData)
+);
