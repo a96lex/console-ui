37b2c4d68be7eec11ecd0d0b8b0b94851e2d63cf
a96lex <a96lex@gmail.com>
2023-08-02 11:28:01 +0200

    add: response display

---
+++ src/lib/previews/Preview.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let data: IPreviewData;
+</script>
+
+<div>
+  <h1>Preview</h1>
+  <p>Here is a preview of your data:</p>
+  <pre>{data.message}</pre>
+</div>
---
+++ src/lib/previews/PreviewContainer.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { previewData } from "../../stores/UserData";
+  import Preview from "./Preview.svelte";
+</script>
+
+{#each $previewData as data}
+  <Preview {data} />
+{/each}
